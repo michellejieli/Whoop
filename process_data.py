ed293ea4cf9cb49c10a81542dc5cca01dc79f9b7
Michelle Li <70456530+michellejieli@users.noreply.github.com>
2022-12-10 22:28:11 +0000

    created lstm and bilstm models

--- process_data.py
+++ process_data.py
@@ -8,16 +8,34 @@ def preprocess_data(tsv_file):
     df['rating'] = df['rating'].apply(lambda x: 1 if x > 5 else 0)
     return df
 
+# split train csv to train and validation csv
+def split_train_val(train_csv):
+    # read train csv file
+    df = pd.read_csv(train_csv)
+    # shuffle data
+    df = df.sample(frac=1).reset_index(drop=True)
+    # split train and validation
+    train_df = df[:int(0.9*len(df))]
+    # shape of train data
+    print("Shape of train data: ", train_df.shape)
+    val_df = df[int(0.9*len(df)):]
+    # shape of validation data
+    print("Shape of validation data: ", val_df.shape)
+    # save to csv file
+    train_df.to_csv('final_data/train.csv', index=False)
+    val_df.to_csv('final_data/valid.csv', index=False)
 if __name__ == '__main__':
     # preprocess data
-    train_data = preprocess_data('data/drugsComTrain_raw.tsv')
-    test_data = preprocess_data('data/drugsComTest_raw.tsv')
+    train_data = preprocess_data('drug_data/drugsComTrain_raw.tsv')
+    test_data = preprocess_data('drug_data/drugsComTest_raw.tsv')
     # print first 5 rows of train data
     print(train_data.head())
     # print shape 
     print("Shape of train data: ", train_data.shape)
     print("Shape of test data: ", test_data.shape)
+    # split train data to train and validation
     # save to csv file
-    train_data.to_csv('data/drugsComTrain.csv', index=False)
-    test_data.to_csv('data/drugsComTest.csv', index=False)
+    train_data.to_csv('drug_data/train.csv', index=False)
+    split_train_val('drug_data/train.csv')
+    test_data.to_csv('final_data/test.csv', index=False)
     
