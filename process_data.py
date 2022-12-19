91d8e1b2fd6553ac8f4b6a4db53f3b159b6caa95
Michelle Li <70456530+michellejieli@users.noreply.github.com>
2022-12-19 04:50:30 +0000

    create new cleaned data

--- process_data.py
+++ process_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 # get review and rating from data tsv files
 # save review and rating to data csv files
@@ -7,8 +10,21 @@ def preprocess_data(tsv_file):
     df = pd.read_csv(tsv_file, sep="\t", usecols=["review", "rating"])
     # convert rating to 0 or 1
     df["rating"] = df["rating"].apply(lambda x: 1 if x > 5 else 0)
+    print("Done preprocessing data")
     return df
 
+# remove stop words, punctuations, and numbers
+# apply stemming
+def clean_text(text):
+    # remove stop words
+    text = " ".join([word for word in str(text).split() if word not in stopwords.words("english")])
+    # remove punctuations
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    # remove numbers
+    text = text.translate(str.maketrans("", "", string.digits))
+    # apply stemming
+    text = " ".join([PorterStemmer().stem(word) for word in text.split()])
+    return text
 
 # split train csv to train and validation csv
 def split_train_val(train_csv):
@@ -26,12 +42,19 @@ def split_train_val(train_csv):
     # save to csv file
     train_df.to_csv("final_data/train.csv", index=False)
     val_df.to_csv("final_data/valid.csv", index=False)
+    # print done splitting train and validation
+    print("Done splitting train and validation")
 
 
 if __name__ == "__main__":
     # preprocess data
     train_data = preprocess_data("drug_data/drugsComTrain_raw.tsv")
     test_data = preprocess_data("drug_data/drugsComTest_raw.tsv")
+    # clean text
+    train_data["review"] = train_data["review"].apply(clean_text)
+    test_data["review"] = test_data["review"].apply(clean_text)
+    # print done cleaning text
+    print("Done cleaning text")
     # print first 5 rows of train data
     print(train_data.head())
     # print shape
